6f3da7b2eb75e0c2a95aed120988f12f6b7a793b
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-02-02 13:05:32 -0300

    refact: componentes refatorados

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,10 +1,28 @@
-export default function Section(props) {
+import ItemList from "./ItemList";
+
+export default function Section({ title, subtitle, className, children, list }) {
     return (
         <section>
             <div>
-                <h2>{props.title}</h2>
-                <p>{props.subtitle}</p>
-                <ul className={props.className}>{props.children}</ul>
+                <h2>{title}</h2>
+                <p>{subtitle}</p>
+                <ul className={className}>
+                    {
+                        list.map((item, key) => {
+                            const { url, imgUrl, alt } = item
+                            return (
+
+                                <ItemList
+                                    url={url}
+                                    imgUrl={imgUrl}
+                                    alt={alt}
+                                    key={key}
+                                />
+                            )
+                        })
+                    }
+                    {children}
+                </ul>
             </div>
         </section>
     );
